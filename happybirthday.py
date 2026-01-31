c238110fd2a05e5111e249f1f91752a679b45d2d
moon <mamoonu@hotmail.com>
2026-01-31 19:23:30 +0000

    Update happybirthday.py

--- happybirthday.py
+++ happybirthday.py
@@ -2,5 +2,5 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-sense.show_message("HAPPY BIRTHDAY ERICA", text_colour=[255, 0, 255], back_colour=[0, 0, 0], scroll_speed=0.02) 
+sense.show_message("HAPPY BIRTHDAY ERICA", text_colour=[255, 0, 255], back_colour=[0, 0, 0], scroll_speed=0.06) 
 sense.clear()
