ae7395d61d93970bd6ad84083459f27a307a2d30
moon <mamoonu@hotmail.com>
2026-01-31 19:12:17 +0000

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,89 @@
+from sense_hat import SenseHat
+import time
+import random
+
+# Initialize
+sense = SenseHat()
+sense.clear()
+
+# Colors
+SNAKE_COLOR = [0, 255, 0]  # Green
+FOOD_COLOR = [255, 0, 0]   # Red
+EMPTY_COLOR = [0, 0, 0]    # Off
+
+# Snake initial state
+snake = [(4, 4), (3, 4), (2, 4)]
+direction = "RIGHT"
+
+# Food
+food = None
+def place_food():
+    while True:
+        f = (random.randint(0, 7), random.randint(0, 7))
+        if f not in snake:
+            return f
+
+food = place_food()
+
+# Joystick handler
+def handle_joystick(event):
+    global direction
+    if event.action != 'pressed':
+        return
+    if event.direction == 'up' and direction != 'DOWN':
+        direction = 'UP'
+    elif event.direction == 'down' and direction != 'UP':
+        direction = 'DOWN'
+    elif event.direction == 'left' and direction != 'RIGHT':
+        direction = 'LEFT'
+    elif event.direction == 'right' and direction != 'LEFT':
+        direction = 'RIGHT'
+
+sense.stick.direction_any = handle_joystick
+
+# Move snake
+def move_snake():
+    global snake, food
+    head_x, head_y = snake[0]
+    if direction == 'UP':
+        head_y -= 1
+    elif direction == 'DOWN':
+        head_y += 1
+    elif direction == 'LEFT':
+        head_x -= 1
+    elif direction == 'RIGHT':
+        head_x += 1
+
+    new_head = (head_x, head_y)
+
+    # Collision check
+    if new_head in snake or not (0 <= head_x <= 7 and 0 <= head_y <= 7):
+        return False
+
+    snake.insert(0, new_head)
+
+    if new_head == food:
+        food = place_food()
+    else:
+        snake.pop()
+
+    return True
+
+# Draw function
+def draw():
+    sense.clear()
+    for segment in snake:
+        sense.set_pixel(segment[0], segment[1], SNAKE_COLOR)
+    sense.set_pixel(food[0], food[1], FOOD_COLOR)
+
+# Main game loop
+game_over = False
+speed = 0.5  # Seconds per move
+while not game_over:
+    game_over = not move_snake()
+    draw()
+    time.sleep(speed)
+
+# Game over message
+sense.show_message("GAME OVER", text_colour=[255,0,0])
+sense.clear()
